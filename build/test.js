b71fd5ac058c0b92aa79a9e025fce19f4f14dcd8
uyoung <zzyouyang@qq.com>
2018-02-08 02:49:59 +0800

    first commit

---
+++ build/test.js
@@ -0,0 +1,6 @@
+const path = require('path')
+let myPath2 = path.join(__dirname, '../dist')
+let myPath1 = path.join(__dirname, '../client/app.js')
+console.log(__dirname) // D:\myProgram\test
+console.log('myPath2：', myPath2) // D:\myProgram\test\img\so
+console.log('myPath1：', myPath1)
---
+++ client/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default () => <div>This is app1323</div>;
+
+// export default class App extends React.Component {
+//   render() {
+
+//   }
+// }
