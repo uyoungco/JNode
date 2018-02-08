2200af721a86a313a1653054d13a1ab3b9cc434b
uyoung <zzyouyang@qq.com>
2018-02-08 20:32:04 +0800

    更佳规范的eslint

--- client/app.js
+++ client/app.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader' //  eslint-disable-line
-import App from './views/App.jsx'
+import App from './views/App'
 
 // ReactDOM.hydrate(<App />,document.getElementById('root'))
 
